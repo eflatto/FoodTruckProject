d2807c8293e4e65343a373de2e931ee17f83ad9f
Edwin <Flattoedwin@gmail.com>
2023-01-20 21:10:17 -0500

    user story1 and userstory2 complete

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,13 +1,51 @@
 package com.skilldistillery.foodtruck.app;
 
+import java.util.Scanner;
+
 import com.skilldistillery.foodtruck.entities.Foodtruck;
 
 public class FoodTruckApp {
+	// This main foodtruck does not pass in ID value for any foodtruck
+
+	// fllet o foodtruck
 	private Foodtruck[] fleet;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+			FoodTruckApp fta = new FoodTruckApp(); 
+	
+			Scanner sc = new Scanner(System.in);
+			fta.fleet = new Foodtruck [5];
+			boolean keepGoing = true;
+			
+
 		
+		do {
+			
+			System.out.println("Please enter the name, food type, and rating of up to 5 food trucks: ");
+			
+			for (int i = 0; i < 5; i++) {
+				System.out.print("Food Truck Name:");
+				String name = sc.next();
+				if (name.equalsIgnoreCase("quit")) {
+					System.out.print("goodbye");
+					keepGoing = false;
+					break;	
+				}
+				System.out.print("Food Type:");
+				String foodType = sc.next();
+				System.out.print("Rating:");
+				int rating = sc.nextInt();
+				
+				
+
+				fta.fleet[i] = new Foodtruck(name, foodType, rating);
+
+				System.out.println(fta.fleet[i]);
+
+			}
+
+		} while (keepGoing);
 
 	}
 
--- src/com/skilldistillery/foodtruck/entities/Foodtruck.java
+++ src/com/skilldistillery/foodtruck/entities/Foodtruck.java
@@ -2,6 +2,39 @@ package com.skilldistillery.foodtruck.entities;
 
 public class Foodtruck {
 
-	private static int nextTruckId;
-	private int id;
+	private static int nextTruckId =100;
+	private int id ;
+	private int rating;
+	private String name;
+	private String foodType;
+		
+	
+	public Foodtruck() {
+		this.id = nextTruckId;
+		nextTruckId++;
+		
+	}
+	
+	
+	public Foodtruck(String name,String foodType,int rating) {
+		this.name = name;
+		this.foodType = foodType;
+		this.rating = rating;
+		this.id=nextTruckId;
+		nextTruckId++;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	@Override
+	public String toString() {
+		return "Foodtruck [id=" + id + ", rating=" + rating + ", name=" + name + ", foodType=" + foodType + "]";
+	}
+
+	
+	
+	
+	
+	
+	
 }
